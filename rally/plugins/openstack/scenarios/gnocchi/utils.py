47473d99262213acd7dcfaa692208b9979c4c1a2
Sumant Murke <sumantmurke@gmail.com>
2016-02-02 19:58:29 +0000

    updated import

--- rally/plugins/openstack/scenarios/gnocchi/utils.py
+++ rally/plugins/openstack/scenarios/gnocchi/utils.py
@@ -1,7 +1,7 @@
 
 
 
-import rally.plugins.openstack import scenario
+from rally.plugins.openstack import scenario
 
 class GnocchiScenario(scenario.OpenstackScenario):
     """Base class for gnocchi scenarios and basic atomic actions """
