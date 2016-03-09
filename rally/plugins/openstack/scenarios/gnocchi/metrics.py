c09eab632e9d6ed442c607ec1a0534ae9129b864
Sumant Murke <sumantmurke@gmail.com>
2016-03-09 15:49:37 +0000

    adding show metric handler

--- rally/plugins/openstack/scenarios/gnocchi/metrics.py
+++ rally/plugins/openstack/scenarios/gnocchi/metrics.py
@@ -6,6 +6,9 @@ from rally.task import validation
 
 class GnocchiMetric(gnocchiutils.GnocchiScenario):
     """Benchmarking scenarios for Gnocchi Metric API"""
+
+
+
     @validation.required_services(consts.Service.GNOCCHI)
     @validation.required_openstack(users=True)
     @scenario.configure()
@@ -13,5 +16,13 @@ class GnocchiMetric(gnocchiutils.GnocchiScenario):
         """list metrics by querrying the resources"""
 
 
-        #query = self._make_general_query()
         self._list_metrics()
+
+
+    @validation.required_services(consts.Service.GNOCCHI)
+    @validation.required_openstack(users=True)
+    @scenario.configure()
+    def show_metrics(self):
+        """shows details of specific metric by querying"""
+
+        pass
